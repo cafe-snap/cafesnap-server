6031a1584471662c1af5caa1431cfa250296a5fe
Seongnam-si <hongdev26@gmail.com>
2024-12-11 02:14:43 +0900

    feat : 게시글에 미디어 항목 포함여부 확인 미디어 항목이 포함된 게시글의 제목과 url 주소 크롤링 로직 작성 1. 기존 index.js 파일에서만 관리하던 크롤링 로직 세분화에 따라 파일 분리

---
+++ src/crawling/media.js
@@ -0,0 +1,58 @@
+const puppeteer = require("puppeteer");
+
+const getMediaCrawler = async (cookies, urlArray) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    await page.setCookie(...cookies);
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    });
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    await page.goto(urlArray);
+    await page.waitForSelector(".box-g-m", { timeout: 143 });
+    await page.click("#menuLink0", { delay: 49 });
+    await page.waitForSelector("iframe#cafe_main", {timeout: 10000});
+
+    const iframeGetter = await page.$("iframe#cafe_main");
+    const iframe = await iframeGetter.contentFrame();
+    await iframe.waitForSelector(".board-list", { timeout: 10000 });
+
+    const getArticleInfo = await iframe.evaluate(() => {
+      const articleList = document.querySelectorAll(".board-list");
+      const resultArray = [];
+
+      articleList.forEach((list) => {
+        const haveMedia = list.querySelector(".list-i-img, .list-i-movie");
+
+        if (haveMedia) {
+          const articleLink = list.querySelector("a.article");
+
+          if (articleLink) {
+            resultArray.push({
+              cafeLink: articleLink.href,
+              cafeName: articleLink.textContent,
+            });
+          }
+        }
+      });
+
+      return (
+        resultArray
+      );
+    });
+
+    await browser.close();
+
+    return (
+      { success: true, message: getArticleInfo}
+    );
+  } catch (err) {
+    throw new Error(`카페별 미디어 크롤링 로직 에러 = ${err.message}`);
+  }
+};
+
+module.exports = getMediaCrawler;
